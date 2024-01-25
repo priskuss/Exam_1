c9ff3d99962926decc0b450aeefc1d54400cbdcb
unknown <aniamilton129@gmail.com>
2024-01-25 20:41:50 +0200

    solved task3

--- task3.cs
+++ task3.cs
@@ -1,31 +1,20 @@
-// Your program should start at this line.
-/*
-Example:
-Move();
-Move();
-Move();
-Turn();
-Move();
-Move();
-Turn();
-Turn();
-Turn();
-Move();
-*/
-
-
 #region Basic functions
 // These functions are just her to make your intelisense work. 
 // They only have a conceptual function.
 
 void Move()
 {
-    // Moves the car 1 cell in the direction it is heading. 
+    // Moves the car until the next cell is a wall.
 }
 
-void Turn()
+void TurnRight()
 {
     // Turns the car 90 deg clockwise.
+}
+
+void TurnLeft()
+{
+    // Turns the car 90 deg counterclockwise.
 }
 
 bool Peek()
@@ -37,7 +26,30 @@ bool Peek()
 bool AtGoal()
 {
     // Returns true if the current cell is the goal cell.
-    return true; // just a placholder
+    return true; // Just a placeholder value.
 }
 
+bool RightTurn()
+{
+    // Represents the direction of the next turn.
+    // Returns true if the next turn should be to the right, otherwise false.
+    return true; // Just a placeholder value. 
+}
 #endregion
+
+void Main()
+{
+    while (!AtGoal())
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (RightTurn)
+                TurnRight();
+            else
+                TurnLeft();
+
+            Move();
+        }
+        RightTurn = !RightTurn;
+    }
+}
