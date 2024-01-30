a0057a88780ea1c0b134dc91e80cdaf7c52c9965
unknown <aniamilton129@gmail.com>
2024-01-30 17:27:31 +0200

    maybe solved task4

--- task4.cs
+++ task4.cs
@@ -12,13 +12,6 @@ void Turn()
     // Turns the car 90 deg clockwise.
 }
 
-void Move()
-{
-    (int, int) = NextPosition();
-    walkedCells.Add((int, int));
-    // Moves the car 1 cell in the direction it is heading.
-    // Track the walked cells.
-}
 bool Peek()
 {
     // Returns true if the next cell is open, otherwise false.
@@ -31,19 +24,12 @@ bool AtGoal()
     return true; // Just a placeholder value.
 }
 
-void Backtrack()
-{
-    if (walkedCells.Count > 1)
-    {
-        walkedCells.RemoveAt(walkedCells.Count - 1);
-        (int, int) = walkedCells.Last();
-        Move();
-    }
-}
 
 #endregion
 
 List<(int, int)> walkedCells = new List<(int, int)>();
+(int, int) = NextPosition();
+walkedCells.Add((int, int));
 
 void Main()
 {
@@ -55,7 +41,12 @@ void Main()
         }
         else
         {
-            Backtrack();
+            if (walkedCells.Count > 1)
+            {
+                walkedCells.RemoveAt(walkedCells.Count - 1);
+                (int, int) = walkedCells.Last();
+                Move();
+            }
             Turn();
         }
     }
