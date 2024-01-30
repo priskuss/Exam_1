774bfa37074643e34611872101144d44756ddca6
unknown <aniamilton129@gmail.com>
2024-01-30 18:56:14 +0200

    new solution

--- task5.cs
+++ task5.cs
@@ -2,20 +2,80 @@
 // These functions are just her to make your intelisense work. 
 // They only have a conceptual function.
 
-void TeleportToGoal()
+void Move()
 {
-    // Teleport the car directly to the goal.
-    SetCarPosition(2, 15);
+    // Moves the car 1 cell in the direction it is heading. 
 }
 
-void SetCarPosition(int, int)
+void Turn()
 {
-    // Sets the car's position to the specified coordinates.
+    // Turns the car 90 deg clockwise.
+}
+void Turns()
+{
+    Turn();
+    Turn();
+    Turn();
+}
+
+bool Peek()
+{
+    // Returns true if the next cell is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+void MoveUntilWall()
+{
+    while (Peek())
+    {
+        Move();
+    }
+}
+
+bool AtGoal()
+{
+    // Returns true if the current cell is the goal cell.
+    return true; // just a placeholder.
 }
 
 #endregion
-
 void Main()
 {
-    TeleportToTheGoal();
+    int peekCount = 0;
+
+    while (!AtGoal())
+    {
+        MoveUntilWall();
+
+        peekCount++;
+
+        if (peekCount == 1 || peekCount == 3 || peekCount == 5 || peekCount == 6 || peekCount == 7 || peekCount == 8)
+        {
+            Turn();
+        }
+        else if (peekCount == 2 || peekCount == 4)
+        {
+            Turns();
+        }
+    }
 }
+
+/*
+MoveUntilWall();
+Turn();
+MoveUntilWall();
+Turns();
+MoveUntilWall();
+Turn();
+MoveUntilWall();
+Turns();
+MoveUntilWall();
+Turn();
+MoveUntilWall();
+Turn();
+MoveUntilWall();
+Turn();
+MoveUntilWall();
+Turn();
+MoveUntilWall();
+*/
